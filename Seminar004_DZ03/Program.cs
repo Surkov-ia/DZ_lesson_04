d8d914e77184303bd1832b223fae95a8f6397b60
Gocha <Frost220v@gmail.com>
2023-05-31 19:07:25 +0500

    программа выводит рандлмный массив с указаной длиной и числами от 1 до 100

---
+++ Seminar004_DZ03/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите длину массива = ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Массив = ");
+void CreateArray()
+{
+    int[] arr = new int[num];
+    int count = 0;
+    while(count < arr.Length)
+    {
+        arr [count] = new Random().Next(0,101);
+        Console.Write($"{arr[count]} ");
+        count++;
+    }
+}
+CreateArray();

c2ad9f252ff7f990ad4928b3ffde21cef0637235
Gocha <Frost220v@gmail.com>
2023-05-31 19:05:58 +0500

    программа показывает сумму введеных чисел

---
+++ Seminar004_DZ02/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 27: Напишите программу, которая принимает
+//  на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число = ");
+int num = Convert.ToInt32(Console.ReadLine());
+string txt = Convert.ToString(num);
+
+int Length()
+{
+    int length = txt.Length;
+    return length;
+}
+int size = Length();
+
+
+int Sum()
+{
+    int sum = 0;
+    for(int count = 0;count<=size;count++)
+    {
+        int res = num%10;
+        num = num/10;
+        sum = sum + res;
+    }
+    return sum;
+}
+int result = Sum();
+
+Console.WriteLine($"Сумма чисел = {result}");
+Console.WriteLine($"Количество цифр = {size}");
